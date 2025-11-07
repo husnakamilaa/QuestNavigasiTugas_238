182249fdb107be98127ddd66239457fa0094de23
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-11-07 19:07:52 +0700

    feat: mendeklarasikan variable statis yang ada di resource string

--- app/src/main/java/com/example/navigasitugas/view/ListData.kt
+++ app/src/main/java/com/example/navigasitugas/view/ListData.kt
@@ -2,6 +2,8 @@ package com.example.navigasitugas.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.example.navigasitugas.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -9,5 +11,10 @@ fun ListData(
     OnFormulirBtnClick:()-> Unit,
     OnBackToBeranda:()->Unit
 ) {
-
+    val items = listOf(
+        Pair(stringResource(R.string.nama_lengkap), "Aprilia Kurnianti"),
+        Pair(stringResource(R.string.jenis_kelamin), "Perempuan"),
+        Pair(stringResource(R.string.status), "Lajang"),
+        Pair(stringResource(R.string.alamat), "Sleman")
+    )
 }
