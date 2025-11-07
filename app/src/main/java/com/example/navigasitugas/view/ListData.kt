770a89c4452bb2fdc5445df2b469d910ffc4b356
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-11-07 19:32:52 +0700

    feat: deklarasi card dengan looping semua isi yang ada di items dan buat komponen Text untuk first dengan uppercase

--- app/src/main/java/com/example/navigasitugas/view/ListData.kt
+++ app/src/main/java/com/example/navigasitugas/view/ListData.kt
@@ -1,5 +1,11 @@
 package com.example.navigasitugas.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -10,7 +16,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.navigasitugas.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -31,5 +40,25 @@ fun ListData(
                 title = { Text(text = stringResource(R.string.list), color = Color.White) },
                 colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(R.color.purple_200))
             )
-        }) {  }
+        }) { isiRuang->
+        Column(modifier = Modifier
+            .background(Color(0xFFE8D3ED))
+            .padding(isiRuang),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium))) {
+            items.forEach { item->
+                    Card(modifier = Modifier
+                        .padding(all=16.dp),
+                        elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+                        colors = CardDefaults.cardColors(
+                            containerColor = Color.White))
+                    {
+                        Column {
+                            Text(text = item.first.uppercase(),
+                                fontSize = 16.sp)
+                        }
+
+                    }
+            }
+        }
+    }
 }
