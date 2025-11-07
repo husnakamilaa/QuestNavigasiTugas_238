f99419724598ceece941687e1cb206368f06e8a6
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-11-07 19:02:33 +0700

    feat: manggil api material3Api, buat function, dan deklarasi button ke beranda dan Formulir

--- app/src/main/java/com/example/navigasitugas/view/ListData.kt
+++ app/src/main/java/com/example/navigasitugas/view/ListData.kt
@@ -1,2 +1,13 @@
 package com.example.navigasitugas.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ListData(
+    OnFormulirBtnClick:()-> Unit,
+    OnBackToBeranda:()->Unit
+) {
+
+}
