3b984e0fe7365360bbf582515e191da9fd6d1fb5
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-11-07 17:54:14 +0700

    feat: membuat navigasi untuk halaman beranda, list data, dan formulir

--- app/src/main/java/com/example/navigasitugas/Navigasi.kt
+++ app/src/main/java/com/example/navigasitugas/Navigasi.kt
@@ -1,2 +1,52 @@
 package com.example.navigasitugas
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+
+enum class Navigasi {
+    Beranda,
+    ListData,
+    Formulir
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+) {
+    Scaffold { isiRuang->
+        NavHost(
+            navController = NavController,
+            startDestination = Navigasi.Beranda.name,
+            modifier = Modifier.padding(paddingValues = isiRuang)
+        ) {
+            composable(route = Navigasi.Beranda.name) {
+                Beranda(
+                    OnListDataBtnClick = {
+                        navController.navigate(route = Navigasi.ListData.name)
+                    }
+                )
+            }
+            composable(route = Navigasi.ListData.name) {
+                ListData(
+                    OnFormulirBtnClick = {
+                        navController.navigate(route = Navigasi.Formulir.name)
+                    }
+                    OnBackBerandaBtnClick = {backToBeranda(navController)}
+                )
+            }
+            composable(route= Navigasi.Formulir.name) {
+                Formulir(
+                    onBackListDataBtnClick = {backToListData(navController)}
+                )
+            }
+        }
+    }
+}
+
